e9424cdfd6e034cb0bd256a4447bbe97c5522c74
PIKA CHIU <user@Userde-MacBook-Pro.local>
2017-02-08 20:28:32 +0800

    Initial Commit

---
+++ monicaapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  monicaapp
+//
+//  Created by User on 2017/2/8.
+//  Copyright © 2017年 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
